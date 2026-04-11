a74469968afe01e60c2f101ee716277cbce962e2
Stephen Waits <steve@waits.net>
2026-04-11 16:42:47 -0600

    adapters: php, ruby, lua

--- lua/tiny-debugger/adapters/ruby.lua
+++ lua/tiny-debugger/adapters/ruby.lua
@@ -1,1 +1,33 @@
-return {}
+local loader = require("tiny-debugger.adapters.init")
+
+local M = {}
+
+function M.register(dap)
+  local rdbg = loader.find_executable({ "rdbg" })
+  if not rdbg then return end
+
+  loader.register(dap, "rdbg", {
+    type = "server",
+    port = "${port}",
+    executable = {
+      command = rdbg,
+      args = { "-n", "--open", "--port", "${port}", "-c", "--", "ruby", "${file}" },
+    },
+  }, {
+    {
+      type = "rdbg",
+      request = "launch",
+      name = "Launch file",
+      command = "ruby",
+      script = "${file}",
+    },
+    {
+      type = "rdbg",
+      request = "attach",
+      name = "Attach",
+      localfsMap = "${workspaceFolder}",
+    },
+  }, { "ruby" })
+end
+
+return M
