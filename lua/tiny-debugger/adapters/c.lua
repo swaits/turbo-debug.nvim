38580100fe44591d2bd0b924b12a7534242e1c24
Stephen Waits <steve@waits.net>
2026-04-11 16:41:47 -0600

    adapters: c/cpp/rust/swift via codelldb

--- lua/tiny-debugger/adapters/c.lua
+++ lua/tiny-debugger/adapters/c.lua
@@ -1,1 +1,34 @@
-return {}
+local loader = require("tiny-debugger.adapters.init")
+
+local M = {}
+
+function M.register(dap)
+  local codelldb = loader.find_executable({ "codelldb" })
+  if not codelldb then return end
+
+  local adapter = {
+    type = "server",
+    port = "${port}",
+    executable = {
+      command = codelldb,
+      args = { "--port", "${port}" },
+    },
+  }
+
+  local configurations = {
+    {
+      type = "codelldb",
+      request = "launch",
+      name = "Launch executable",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      end,
+      cwd = "${workspaceFolder}",
+      stopOnEntry = false,
+    },
+  }
+
+  loader.register(dap, "codelldb", adapter, configurations, { "c", "cpp", "rust", "swift" })
+end
+
+return M
