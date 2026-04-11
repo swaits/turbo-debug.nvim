11c4509fb061c6fc9c89b7adc828740554747400
Stephen Waits <steve@waits.net>
2026-04-11 16:42:04 -0600

    adapters: javascript/typescript via js-debug-adapter

--- lua/tiny-debugger/adapters/javascript.lua
+++ lua/tiny-debugger/adapters/javascript.lua
@@ -1,1 +1,43 @@
-return {}
+local loader = require("tiny-debugger.adapters.init")
+
+local M = {}
+
+function M.register(dap)
+  local js_debug = loader.find_executable({ "js-debug-adapter" })
+  if not js_debug then return end
+
+  local adapter = {
+    type = "server",
+    host = "localhost",
+    port = "${port}",
+    executable = {
+      command = js_debug,
+      args = { "${port}" },
+    },
+  }
+
+  local configurations = {
+    {
+      type = "pwa-node",
+      request = "launch",
+      name = "Launch file",
+      program = "${file}",
+      cwd = "${workspaceFolder}",
+    },
+    {
+      type = "pwa-node",
+      request = "attach",
+      name = "Attach",
+      processId = function()
+        return require("dap.utils").pick_process()
+      end,
+      cwd = "${workspaceFolder}",
+    },
+  }
+
+  loader.register(dap, "pwa-node", adapter, configurations, {
+    "javascript", "typescript", "javascriptreact", "typescriptreact",
+  })
+end
+
+return M
